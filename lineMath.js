2cd566fcd659228b856ef7e820b43ec7a97bfbfc
Daniel Mahal <danielmahal@gmail.com>
2017-06-29 11:25:53 +0200

    Punch’em away

--- lineMath.js
+++ lineMath.js
@@ -2,6 +2,10 @@ function lineLength ([[x1, y1], [x2, y2]]) {
   return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
 }
 
+function lineAngle ([[x1, y1], [x2, y2]]) {
+  return Math.atan2(y2 - y1, x2 - x1)
+}
+
 function linesIntersect ([[x1, y1], [x2, y2]], [[x3, y3], [x4, y4]]) {
   const x = ((x1 * y2 - y1 * x2) * (x3 - x4) - (x1 - x2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
   const y = ((x1 * y2 - y1 * x2) * (y3 - y4) - (y1 - y2) * (x3 * y4 - y3 * x4)) / ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4))
